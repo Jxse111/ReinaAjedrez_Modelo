7679a65c1bedb5751c312288fe2620b5b93467a2
diurno <diurno@pc06>
2022-11-14 14:31:11 +0100

    He agregado al enum de color el constructor de la cadenaAMosrtar

--- src/main/java/org/iesalandalus/programacion/reinaajedrez/modelo/Color.java
+++ src/main/java/org/iesalandalus/programacion/reinaajedrez/modelo/Color.java
@@ -1,7 +1,13 @@
 package org.iesalandalus.programacion.reinaajedrez.modelo;
 
 public enum Color {
-BLANCO,
-NEGRO;
-private String cadenaAMostrar;
-} 
+	BLANCO, 
+	NEGRO;
+
+	private String cadenaAMostrar;
+
+	private Color(String cadenaAMostrar) {
+		this.cadenaAMostrar = cadenaAMostrar;
+	}
+
+}
