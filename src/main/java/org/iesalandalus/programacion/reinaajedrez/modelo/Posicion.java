cb1e297cbcbf0d6d523309e7dcdd4ec4612b665a
diurno <diurno@pc06>
2022-11-15 12:06:23 +0100

    llamo a los set de columna y fila para que en caso de fallo salte la excepción en caso de que los parámetros sean incorrectos.

--- src/main/java/org/iesalandalus/programacion/reinaajedrez/modelo/Posicion.java
+++ src/main/java/org/iesalandalus/programacion/reinaajedrez/modelo/Posicion.java
@@ -3,28 +3,36 @@ package org.iesalandalus.programacion.reinaajedrez.modelo;
 public class Posicion {
 	private int fila;
 	private char columna;
-	
-public int getFila() {
+
+	public int getFila() {
 		return fila;
 	}
+
 	public void setFila(int fila) {
-		if(fila >=1 && fila <=8) {
-		this.fila = fila;
-		}else {
-			throw new IllegalArgumentException("Se ha salido de la casilla");		
+		if (fila >= 1 && fila <= 8) {
+			this.fila = fila;
+		} else {
+			throw new IllegalArgumentException("Se ha salido de la casilla");
 		}
 	}
-		public char getColumna() {
+
+	public char getColumna() {
 		return columna;
 	}
+
 	public void setColumna(char columna) {
-		if(columna >='a' && columna <='h') {
-		this.columna = columna;
-		}else {
-			throw new IllegalArgumentException("Se ha salido de la casilla");	
+		if (columna >= 'a' && columna <= 'h') {
+			this.columna = columna;
+		} else {
+			throw new IllegalArgumentException("Se ha salido de la casilla");
+		}
+
 	}
 
+	public Posicion(int fila, char columna) {
+		this.fila = fila;
+		this.columna = columna;
+		setFila(fila);
+		setColumna(columna);
 	}
 }
-
-
