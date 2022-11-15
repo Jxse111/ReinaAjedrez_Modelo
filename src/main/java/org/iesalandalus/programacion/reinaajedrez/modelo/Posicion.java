e769fa40d0a9f3544689cfa980590b2753e74897
diurno <diurno@pc06>
2022-11-15 11:07:40 +0100

    Creo los metodos getters y setters de cada variable y hago los cambios necesarios para que los valores que indique sean correctos y en caso de que no se muestre un mensaje de error.

--- src/main/java/org/iesalandalus/programacion/reinaajedrez/modelo/Posicion.java
+++ src/main/java/org/iesalandalus/programacion/reinaajedrez/modelo/Posicion.java
@@ -1,6 +1,30 @@
 package org.iesalandalus.programacion.reinaajedrez.modelo;
 
 public class Posicion {
-private int fila;
-private char columna;
+	private int fila;
+	private char columna;
+	
+public int getFila() {
+		return fila;
+	}
+	public void setFila(int fila) {
+		if(fila >=1 && fila <=8) {
+		this.fila = fila;
+		}else {
+			throw new IllegalArgumentException("Se ha salido de la casilla");		
+		}
+	}
+		public char getColumna() {
+		return columna;
+	}
+	public void setColumna(char columna) {
+		if(columna >='a' && columna <='h') {
+		this.columna = columna;
+		}else {
+			throw new IllegalArgumentException("Se ha salido de la casilla");	
+	}
+
+	}
 }
+
+
