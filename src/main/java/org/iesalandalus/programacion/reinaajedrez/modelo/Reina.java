12dba11ca922feb03ba4d3427d3cc76554953020
josem <josem@DESKTOP-QUDQGA1>
2022-11-16 18:49:45 +0100

    He creado los getter y setters.

--- src/main/java/org/iesalandalus/programacion/reinaajedrez/modelo/Reina.java
+++ src/main/java/org/iesalandalus/programacion/reinaajedrez/modelo/Reina.java
@@ -3,5 +3,27 @@ package org.iesalandalus.programacion.reinaajedrez.modelo;
 public class Reina {
 	private Color color;
 	private Posicion posicion;
-	
+
+	public Color getColor() {
+		return color;
+	}
+
+	private void setColor(Color color) {
+		if (posicion == null) {
+			throw new NullPointerException("ERROR: El color no puede ser nulo");//No se sabe si es correcto.
+		}
+		this.color = color;
+	}
+
+	public Posicion getPosicion() {
+		return posicion;
+	}
+
+	private void setPosicion(Posicion posicion) {
+		if (posicion == null) {
+			throw new NullPointerException("ERROR: No es posible copiar una posición nula.");//No se sabe si es correcto.
+		}
+		this.posicion = posicion;
+	}
+
 }
