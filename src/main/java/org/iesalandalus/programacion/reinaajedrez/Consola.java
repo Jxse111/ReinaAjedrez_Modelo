b0caa69a30f0da059f467f9835c86a796ce717a1
josem <josem@DESKTOP-QUDQGA1>
2022-11-17 22:40:05 +0100

    He creado el metodo elegirOpcionMenu que muestra un mensaje para elegir la opcion del menú que he creado en el anterior commit.

--- src/main/java/org/iesalandalus/programacion/reinaajedrez/Consola.java
+++ src/main/java/org/iesalandalus/programacion/reinaajedrez/Consola.java
@@ -1,7 +1,6 @@
-package org.iesalandalus.programacion.reinaajedrez.modelo;
-
-import javax.swing.JOptionPane;
-
+package org.iesalandalus.programacion.reinaajedrez;
+import org.iesalandalus.programacion.reinaajedrez.modelo.Color;
+import org.iesalandalus.programacion.reinaajedrez.modelo.Direccion;
 import org.iesalandalus.programacion.utilidades.Entrada;
 
 public class Consola {
