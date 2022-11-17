7859770ddb89948d48d444ae6715d390477043d3
diurno <diurno@pc06>
2022-11-17 13:31:01 +0100

    He creado el método mover para que la reina se pueda mover por el tablero y que en caso de que se salga te salte un error, ademas he agregado el tostring de color y posicion

--- src/main/java/org/iesalandalus/programacion/reinaajedrez/modelo/Reina.java
+++ src/main/java/org/iesalandalus/programacion/reinaajedrez/modelo/Reina.java
@@ -1,6 +1,9 @@
 package org.iesalandalus.programacion.reinaajedrez.modelo;
 
+import javax.naming.OperationNotSupportedException;
+
 public class Reina {
+
 	private Color color;
 	private Posicion posicion;
 
@@ -10,7 +13,7 @@ public class Reina {
 
 	private void setColor(Color color) {
 		if (posicion == null) {
-			throw new NullPointerException("ERROR: El color no puede ser nulo");//No se sabe si es correcto.
+			throw new NullPointerException("ERROR: El color no puede ser nulo");// No se sabe si es correcto.
 		}
 		this.color = color;
 	}
@@ -21,22 +24,39 @@ public class Reina {
 
 	private void setPosicion(Posicion posicion) {
 		if (posicion == null) {
-			throw new NullPointerException("ERROR: No es posible copiar una posición nula.");//No se sabe si es correcto.
+			throw new NullPointerException("ERROR: No es posible copiar una posición nula.");// No se sabe si es
+																								// correcto.
 		}
 		this.posicion = posicion;
 	}
+
 	public Reina() {
 		color = color.BLANCO;
-		posicion = new Posicion (1,'d');
+		posicion = new Posicion(1, 'd');
 	}
+
 	public Reina(Color color) {
 		if (color == Color.NEGRO) {
-			posicion = new Posicion(1,'d');
+			posicion = new Posicion(1, 'd');
+			reinaBlanca = new Reina(Color.BLANCO);
 			this.color = color;
-	}else  if (color == Color .BLANCO){
-		posicion = new Posicion(1,'d');
+		} else if (color == Color.BLANCO) {
+			posicion = new Posicion(1, 'd');
+			reinaNegro = new Reina(Color.NEGRO);
 			this.color = color;
+		}
 	}
-}
+
+	public void mover(Direccion direccion, int Pasos)  throws OperationNotSupportedException {
+		if  (direccion == null) {
+			throw new NullPointerException("Los pasos no pueden ser nulos");
+		}else if (Pasos <= 1 && Pasos >= 7 ){
+			throw new OperationNotSupportedException("Los pasos salen del tablero");
+		}}
+
+	@Override
+	public String toString() {
+		return String.format("Reina [color=%s, posicion=%s]", color, posicion);
+	}
 
 }
